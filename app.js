bb588c9958d9e7fe01d611d4a75448eff337c0f8
Justin <jusglinton@gmail.com>
2021-07-24 15:05:44 -0400

    contact js

--- app.js
+++ app.js
@@ -25,3 +25,76 @@ function login(){
     LoginForm.style.transform = "translateX(300px)";
     Indicator.style.transform = "translateX(0px)";
 }
+
+const isValidEmail = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+const isValidPhone = (phone) => {
+    const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+    return re.test(String(phone).toLowerCase());
+  };
+
+const form = document.querySelector('form');
+const thankYou = document.querySelector('.thank-you');
+const nameInput = document.querySelector('input[name="name"]');
+const emailInput = document.querySelector('input[name="email"]');
+const phoneInput = document.querySelector('input[name="phone"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+
+const inputs = [nameInput, emailInput, phoneInput, messageInput];
+  
+let isFormValid = false
+let isValidationOn = false
+
+function restElm(elm){
+    elm.classList.remove('invalid');
+    elm.nextElementSibling.classList.add("hidden");
+};
+
+function invalidateElm(elm){
+    elm.classList.add('invalid');
+    elm.nextElementSibling.classList.remove("hidden");
+}
+
+function validateInputs(){
+    if(!isValidationOn) return;
+
+    isFormValid = true;
+    inputs.forEach(restElm);
+     
+    if(!nameInput.value){
+         isFormValid = false;
+         invalidateElm(nameInput);
+      }
+
+    if(!isValidEmail(emailInput.value)){
+         isFormValid = false;
+         invalidateElm(emailInput);
+      }
+    if(!isValidPhone(phoneInput.value)){
+         isFormValid = false;
+         invalidateElm(phoneInput);
+      }
+    if(!messageInput.value){
+         isFormValid = false;
+         invalidateElm(messageInput);
+      }
+  };
+
+form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      isValidationOn = true;
+      validateInputs();
+      if (isFormValid){
+        form.remove();
+        thankYou.classList.remove('hidden');
+      }   
+  });
+
+inputs.forEach((input) => {
+    input.addEventListener("input", () => {
+        validateInputs();
+    });
+});
+
